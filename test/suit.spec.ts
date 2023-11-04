6dcb14cd5066898374318fed5c36a9ee47792952
mehmaj <31012967+mehmaj@users.noreply.github.com>
2023-11-04 13:53:59 +0330

    Create suit.spec.ts

---
+++ test/suit.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect, type Page,chromium } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  !@beforeEach
+});
+
+!@testCase
+
+test.afterEach(async ({ page }) => {
+  !@afterEach
+});
